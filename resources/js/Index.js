029f13eeffcbaf905e751440de41193696c55823
kjaneczko <github@kacperjaneczko.pl>
2020-06-27 16:49:23 +0200

    author

--- resources/js/Index.js
+++ resources/js/Index.js
@@ -158,7 +158,7 @@ class Index extends React.Component {
                 <div className="row justify-content-center">
                     <div className="col-md-8">
                         <div className="card">
-                            <div className="card-header">Currency Exchange App</div>
+                            <div className="card-header">Currency Exchange App by Kacper Janeczko</div>
                             <div className="card-body">
                                 <div className="row">
                                     <div className="col">
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="author" content="Kacper Janeczko">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
 
-        <title>Currency Exchange APP</title>
+        <title>Currency Exchange App by Kacper Janeczko</title>
     </head>
     <body>
         <div id="app" class="mt-30"></div>
